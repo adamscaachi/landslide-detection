382c77c3514fdc4a841e8ee9e386c2c143d69251
adamscaachi <adam.scaachi.14@ucl.ac.uk>
2024-08-16 18:33:29 +0100

    Add argument parser and function to reproduce approach (a)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -6,36 +7,39 @@ from unet import UNet
 from trainer import Trainer
 from evaluator import Evaluator
 
-def train_model(file_name, epochs):
-    bands = ["B4", "B3", "B2", "NDVI", "B13", "B14"]	
-    model = UNet(in_channels=len(bands), out_channels=1)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    data = Data(bands)
-    criterion = nn.BCEWithLogitsLoss() 
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+def train_model(name, data, model, criterion, optimizer, device, epochs):
     train_loader = data.train_loader
     val_loader = data.val_loader
     trainer = Trainer(model, criterion, optimizer, device, train_loader, val_loader)
     trainer.train(epochs)
-    torch.save(model.state_dict(), file_name + '.pth' )
+    torch.save(model.state_dict(), name + '.pth' )
 
-def eval_model(file_name):
-    bands = ["B4", "B3", "B2", "NDVI", "B13", "B14"]	
-    model = UNet(in_channels=len(bands), out_channels=1)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    data = Data(bands)
+def eval_model(name, data, model, device):
+    state_dict = name + '.pth'
     val_loader = data.val_loader
     test_loader = data.test_loader
-    state_dict = file_name + '.pth'
     evaluator = Evaluator(model, state_dict, device, val_loader, test_loader)
 
-if __name__ == "__main__":
-    mode = input("Do you want to train (type 'train') or evaluate (type 'eval') a model? ")
+# Baseline Approach
+def a(mode):
+    name = 'a'
+    bands = ["B4", "B3", "B2", "NDVI", "B13", "B14"]  
+    data = Data(bands)
+    model = UNet(in_channels=len(bands), out_channels=1)  
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     if mode == 'train':
-        file_name = input("Please specify a name for the model: ")
-        epochs = int(input("How many epochs do you want to train the model for? "))
-        train_model(file_name, epochs)
-        print("Training of model " + file_name + " complete!")
+        criterion = nn.BCEWithLogitsLoss() 
+        optimizer = optim.Adam(model.parameters(), lr=1e-4)
+        epochs = 25
+        train_model(name, data, model, criterion, optimizer, device, epochs)
     elif mode == 'eval':
-        file_name = input("Please specify the name of the model to be evaluated: ")
-        eval_model(file_name)
+        eval_model(name, data, model, device)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('approach', type=str, help='Approach abbreviation: e.g. [a] if using the baseline approach.')
+    parser.add_argument('mode', type=str, help='Mode to run the script: [train] or [eval].')
+    args = parser.parse_args()
+    approaches = {"a": a}
+    approach = approaches.get(args.approach)
+    approach(args.mode)
