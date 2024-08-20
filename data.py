a734f84a3d03797650b8d93b7fd9d9c36d91d0d2
adamscaachi <adam.scaachi.14@ucl.ac.uk>
2024-08-20 11:40:21 +0100

    Move dataset class to new file

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 import glob
 import h5py
 import torch
-import random
 import numpy as np
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
+from dataset import AugmentedDataset
 
 class Data:
 
@@ -67,38 +67,3 @@ class Data:
         masks_tensor = torch.tensor(masks, dtype=torch.float32)
         dataset = AugmentedDataset(images_tensor, masks_tensor, augment)
         return DataLoader(dataset, batch_size=64, shuffle=True)
-
-class AugmentedDataset(Dataset):
-
-    def __init__(self, images_tensor, masks_tensor, augment):
-        self.images = images_tensor
-        self.masks = masks_tensor
-        self.augment = augment
-        self.augmentations = [self.flip, self.dropout]
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        image = self.images[idx]
-        mask = self.masks[idx]
-        if self.augment and random.random() > 0.1:
-            image, mask = self.augment_image_and_mask(image, mask)
-        return image, mask
-
-    def augment_image_and_mask(self, image, mask):
-        for function in self.augmentations:
-            if random.random() > 0.5:
-                image, mask = function(image, mask)
-        return image, mask
-
-    def flip(self, image, mask):
-        axis = random.choice([[1], [2], [1, 2]])
-        image = torch.flip(image, axis)
-        mask = torch.flip(mask, axis)
-        return image, mask
-
-    def dropout(self, image, mask):
-        dropout_mask = torch.rand_like(image) > 0.001
-        image = image * dropout_mask
-        return image, mask
